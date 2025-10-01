eaa6166b2a75bd1781187e47e32c1e0e42259516
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-10-01 17:14:04 +0200

    web: Implement reloading of machines table using htmx

--- src/bin/mu-web/main.rs
+++ src/bin/mu-web/main.rs
@@ -82,19 +82,42 @@ struct ActiveUser {
     task: String,
 }
 
-fn handle(mut stream: TcpStream, content: &str) -> Result<()> {
+fn boom(mut stream: TcpStream, content: &str, code: &str) -> Result<()> {
+    let status = "HTTP/1.1 ";
+    let length = content.len();
+    let response = format!("{status} {code}\r\nContent-Length: {length}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes())?;
+    Ok(())
+}
+
+fn get_ok(stream: TcpStream, content: &str) -> Result<()> {
+    let status = "200 OK";
+    boom(stream, content, status)
+}
+
+fn get_not_found(stream: TcpStream, content: &str) -> Result<()> {
+    boom(stream, content, "404 NOT FOUND")
+}
+
+fn handle(stream: TcpStream, base: &str, machines: &str) -> Result<()> {
     let reader = BufReader::new(&stream);
-    let _request = reader
+    let request = reader
         .lines()
         .map(|r| r.unwrap()) // TODO: Properly handle error.
         .take_while(|l| !l.is_empty())
         .collect::<Vec<_>>();
-    // println!("Request: {request:?}");
+    println!("Request: {request:#?}");
+    // TODO: Do this properly with actix or smth.
+    if let Some(get) = request.first().unwrap().strip_prefix("GET")
+        && let Some((addr, _)) = get.trim_start().split_once(char::is_whitespace)
+    {
+        match addr {
+            "/" => get_ok(stream, base)?,
+            "/machines" => get_ok(stream, machines)?,
+            _ => get_not_found(stream, "")?,
+        }
+    };
 
-    let status = "HTTP/1.1 200 OK";
-    let length = content.len();
-    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
-    stream.write_all(response.as_bytes())?;
     Ok(())
 }
 
@@ -112,13 +135,14 @@ fn main() -> Result<()> {
     let template_names = template.get_template_names().collect::<Vec<_>>();
     eprintln!("INFO: Found templates with the following names: {template_names:?}");
     let context = Context::from_serialize(data)?;
-    let content = template.render("index.html", &context)?;
+    let content_base = template.render("index.html", &context)?;
+    let content_machines = template.render("machines.html", &context)?;
 
     let listener = TcpListener::bind("127.0.0.1:5172")?;
     eprintln!("INFO: Listener set up.");
     for stream in listener.incoming() {
         eprintln!("INFO: Caught a stream! {stream:?}");
-        match handle(stream?, &content) {
+        match handle(stream?, &content_base, &content_machines) {
             Ok(_) => {}
             Err(err) => eprintln!("ERROR: {err}"),
         };
